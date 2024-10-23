a664c39f10a717103228a7141529b4e1bf448a11
Ahmed-ozta <ahmedchanane90@gmail.com>
2024-10-23 13:42:23 +0100

    create the review section and modify some old css

--- src/Component/Comment.jsx
+++ src/Component/Comment.jsx
@@ -1,9 +1,12 @@
 import styles from './Comment.module.css'
 
-function Comment() {
+function Comment(props) {
     return (
-        <div>
-            <></>
+        <div className={styles.container}>
+            <h3 className='extraBold whiteText'>{props.title}</h3>
+            <p className='whiteText'>{props.description}</p>
+            <img className={styles.stars} src="Expensrol_image\Group 44.svg" />
+            <img className={styles.profil} src="Expensrol_image\abstract-user-flat-4 4.svg" />
         </div>
     )
 }
