7d1e6ae923f5f48f6fb1e9477e9fe7d9e0f2911e
Ahmed-ozta <ahmedchanane90@gmail.com>
2024-10-23 14:49:56 +0100

    create submain3 and fixing some css for small size media

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -2,6 +2,7 @@ import SubMain1 from './SubMain1.jsx'
 import SubMain2 from './SubMain2.jsx'
 import Features from './Features.jsx'
 import Review from './Review.jsx'
+import SubMain3 from './SubMain3.jsx'
 
 
 function Main() {
@@ -12,7 +13,9 @@ function Main() {
             <SubMain1 />
             <SubMain2 />
             <Features />
+            {/* there is an issues in button arrow not respecting the margin  */}
             <Review />
+            <SubMain3 />
         </>
     )
 }
---
+++ src/Component/SubMain3.jsx
@@ -0,0 +1,13 @@
+import styles from './SubMain3.module.css'
+
+function SubMain3() {
+    return(
+        <div className={styles.main}>
+            <div className={styles.container}>
+                <h2>Your Financial Freedom Starts Here: Simplify Budgeting Today! </h2>
+                <button>Get Started<span>&#8594;</span></button>
+            </div>
+        </div>
+    )
+}
+export default SubMain3
