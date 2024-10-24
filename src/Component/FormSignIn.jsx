952bff7e7502d5d7421d4fb87f85d4fa6bb6255b
Ahmed-ozta <ahmedchanane90@gmail.com>
2024-10-24 17:02:58 +0100

    create sign in page with styling and fix it for all media

--- src/Component/FormSignIn.jsx
+++ src/Component/FormSignIn.jsx
@@ -1,6 +1,33 @@
 
-
+import { useState } from 'react';
+import styles from './FormSignIn.module.css'
 function FormSignIn() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    return (
+
+        <main>
+            <div className={styles.container}>
+                <div className={styles.image}>
+                    <img src="Expensrol_image\Computer login-bro 1.svg" />
+                </div>
+
+                <div className={styles.container}>
+                    <div className={styles.data}>
+                        <h1 className='extraBold blueText'>Welcome Back to Expensrol!</h1>
+                        <p>We're thrilled to see you again! Your journey to financial control continues here. Sign in to access your account, track your expenses, and manage your budget effortlessly. Let’s make your financial goals a reality</p>
+                        <input type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+                        <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+
+                        <a className={styles.links} href="">I don’t have an account.</a>
+                        <button>Create an account</button>
+                    </div>
+
+
+                </div>
+            </div>
+        </main>
+    )
 
 }
 export default FormSignIn
--- src/Component/FormSignUp.jsx
+++ src/Component/FormSignUp.jsx
@@ -17,9 +17,9 @@ function FormSignUp() {
                     <h1 className='extraBold blueText'>Start Your Journey to Financial Control with Expensrol</h1>
                     <p>Join Expensrol today and start managing your expenses effortlessly.
                         Track, categorize, and stay on top of your budget with just a few clicks. Take charge of your money and reach your financial goals.</p>
-                    <input type="text" placeholder='Username' value={username} onChange={(e)=>setUsername(e.target.value)}/>
-                    <input type="password" placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
-                    <input type="password" placeholder='Confirm Password' value={confirm} onChange={(e)=>setPassword(e.target.value)}/>
+                    <input type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <input type="password" placeholder='Confirm Password' value={confirm} onChange={(e) => setConfirm(e.target.value)} />
                     <a className={styles.links} href="">i already have account</a>
                     <button>Create an account</button>
                 </div>
