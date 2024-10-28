a9c3edfce82365be0b08e0a1835230e03abd5bbd
Ahmed-ozta <ahmedchanane90@gmail.com>
2024-10-28 12:54:48 +0100

    fix the styling of dashboard for small devices

--- src/Dashboard/Nav.jsx
+++ src/Dashboard/Nav.jsx
@@ -13,12 +13,27 @@ function Nav(props) {
                     <li className={props.prop5}><a><img src="Expensrol_icon\fi-rs-sign-in.svg" />Log out</a></li>
                 </ul>
                 <div className={styles.support}>
-                    <img src="Expensrol_image\Active Support-bro 1.svg" />j-54tt
+                    <img src="Expensrol_image\Active Support-bro 1.svg" />
                     <h3>need help feel <br />free to ask for support</h3>
                     <button>get Support</button>
                 </div>
             </div>
+            {/* the code for small devices */}
+            <div className={styles.smallContainer}>
+                <img className={styles.logo} src="EXPENSROL LOGO\SVG\ICON.svg" />
+                <ul>
+                    <li ><a id="selcted"><img src="Expensrol_icon\fi-rs-home (1).svg" /></a></li>
+                    <li className={props.prop2}><a><img src="Expensrol_icon\fi-rs-add.svg" /></a></li>
+                    <li className={props.prop3}><a><img src="Expensrol_icon\fi-rs-chat-arrow-grow.svg" /></a></li>
+                    <li className={props.prop4}><a><img src="Expensrol_icon\fi-rs-settings.svg" /></a></li>
+                    <li className={props.prop5}><a><img src="Expensrol_icon\fi-rs-sign-in.svg" /></a></li>
+                </ul>
+                <div className={styles.support}>
+                    <button>get Support</button>
+                </div>
+            </div>
         </nav>
+
     )
 }
 export default Nav
